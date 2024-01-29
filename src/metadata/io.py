b79da061880799ca55b556433b759dedf14c2b24
hoangson <lehoangsonsg@gmail.com>
2024-01-30 10:26:56 +1030

    feat: update documentation for IO

--- src/metadata/io.py
+++ src/metadata/io.py
@@ -10,6 +10,16 @@ class MetadataIO:
     validation_models = {cat: create_sheet_validator(cat) for cat in categories}
 
     def __init__(self, path: str, value_index: int = 3, validate: bool = True):
+        """
+        Metadata class representing metadata read from a MIAPPE metadata file.
+
+        :param path: path to MIAPPE metadata file
+        :param value_index: only relevant for non-investigation sheet - row where
+        first input value is provided. Based on the MIAPPE template example, the
+        firs and second rows are definitions and example; the third row is where
+        the first value is recorded
+        :param validate: whether to validate the metadata using pydantic model
+        """
         self.path = path
         self.value_index = value_index
         self.frames = self.read_excel()
@@ -18,24 +28,49 @@ class MetadataIO:
                 self.validate(cat)
 
     def _read_investigation(self) -> pd.DataFrame:
+        """
+        Read investigation sheet structure and convert to dataframe
+        :return: pd.DataFrame
+        """
         df = pd.read_excel(self.path, sheet_name="Investigation", index_col=0).T
         return df.loc[["Value"], :]
 
     def _read_non_investigation(self, category: str) -> pd.DataFrame:
+        """
+        Read non-investigation sheet structure and convert to dataframe
+        :param category: non investigation sheet - i.e. study, experimental factor
+        :return: pd.DataFrame
+        """
         df = pd.read_excel(self.path, sheet_name=category)
         df.drop(columns="Field", inplace=True)
         return df.iloc[self.value_index:, :]
 
     def read_excel(self) -> dict[str, pd.DataFrame]:
+        """
+        Read MIAPPE metadata file provided as an excel file
+        :return: a dictionary with key being the sheet/category and value being the
+        corresponding dataframe
+
+        """
         frames = {}
         for cat in self.categories:
-            df = self._read_investigation() if cat == "Investigation" else self._read_non_investigation(cat)
+            df = self._read_investigation() if cat == "Investigation" else (
+                self._read_non_investigation(
+                    cat
+                ))
             df = df.replace(np.nan, None)
             df.columns = [col.strip().replace('*', '') for col in df.columns]
             frames[cat] = df
         return frames
 
     def validate(self, category: str) -> None:
+        """
+        Use pydantic validation model to validate the matching dataframe
+
+        :param category: sheet/category - i.e. study, investigation, experimental
+        design
+        :return: raise ValueError if encountered
+        """
         model = self.validation_models[category]
         data = self.frames[category]
         if isinstance(data, pd.Series):
