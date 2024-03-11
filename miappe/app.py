91471df49ab58498f834c824fe0c1205193d241d
hoangson <lehoangsonsg@gmail.com>
2024-03-12 09:30:48 +1030

    feat: set up project structure + add overheads

---
+++ miappe/app.py
@@ -0,0 +1,39 @@
+from typing import AsyncGenerator
+
+from advanced_alchemy.extensions.litestar.plugins.init.config.asyncio import (
+    autocommit_before_send_handler,
+)
+from litestar import Litestar
+from litestar.exceptions import ClientException
+from litestar.plugins.sqlalchemy import SQLAlchemyAsyncConfig, SQLAlchemyPlugin
+from litestar.status_codes import HTTP_409_CONFLICT
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+
+async def provide_transaction(
+        db_session: AsyncSession,
+) -> AsyncGenerator[AsyncSession, None]:
+    try:
+        async with db_session.begin():
+            yield db_session
+    except IntegrityError as exc:
+        raise ClientException(
+            status_code=HTTP_409_CONFLICT,
+            detail=str(exc),
+        ) from exc
+
+
+db_config = SQLAlchemyAsyncConfig(
+    connection_string="sqlite+aiosqlite:///db.sqlite",
+    metadata=Base.metadata,
+    create_all=True,
+    before_send_handler=autocommit_before_send_handler,
+)
+
+app = Litestar(
+    [],
+    dependencies={"transaction": provide_transaction},
+    plugins=[SQLAlchemyPlugin(db_config)],
+)
