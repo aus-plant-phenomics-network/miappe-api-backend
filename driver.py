337907f2c0570143a72cae719f61693eaeda03a1
hoangson <lehoangsonsg@gmail.com>
2024-03-12 14:42:41 +1030

    feat: add adhoc script to populate the db

--- driver.py
+++ driver.py
@@ -5,7 +5,7 @@ from sqlalchemy import event
 from sqlalchemy.engine import Engine
 from sqlalchemy.orm import create_session
 
-from miappe.model import Base, Device
+from miappe.model import Base, Device, Method, Vocabulary
 
 
 @event.listens_for(Engine, "connect")
@@ -20,7 +20,22 @@ Base.metadata.create_all(engine)
 
 session = create_session(engine)
 
-random_id = uuid.uuid4()
-myDevice = Device(name="myDevice", device_type_id=random_id)
-session.add(myDevice)
-# session.commit()
+camera = Vocabulary(name="camera", description="RGB Camera")
+nikon_camera = Device(name="NIKON A1", description="NIKON A1", brand="NIKON")
+sony_camera = Device(name="SONY S1", description="SONY A1", brand="SONY")
+camera.device.append(nikon_camera)
+camera.device.append(sony_camera)
+
+mobile_phone = Vocabulary(name="mobile_phone", description="Mobile Phone")
+apple_phone = Device(name="apple iphone", description="apple iphone", brand="apple")
+mobile_phone.device.append(apple_phone)
+
+taking_photo = Vocabulary(name="photo_capture", description="Method of taking pictures")
+iphone_capture = Method(name="iphone_photo_capture", description="Taking photo using an iphone")
+iphone_capture.device = apple_phone
+taking_photo.method.append(iphone_capture)
+
+session.add(camera)
+session.add(mobile_phone)
+session.add(taking_photo)
+session.commit()
