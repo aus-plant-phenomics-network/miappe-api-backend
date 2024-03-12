3de49b392b5ace7a0cd44b45b3e8fc407df9cd23
hoangson <lehoangsonsg@gmail.com>
2024-03-12 14:43:37 +1030

    feat: add method table

---
+++ miappe/model/method.py
@@ -0,0 +1,22 @@
+from typing import Optional, TYPE_CHECKING
+from uuid import UUID
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import mapped_column, Mapped, relationship
+
+from miappe.model.base import Base
+
+if TYPE_CHECKING:
+    from miappe.model.device import Device
+    from miappe.model.vocabulary import Vocabulary
+
+
+class Method(Base):
+    __tablename__ = "method_table"
+
+    # Relationships:
+    method_type_id: Mapped[Optional[UUID]] = mapped_column(ForeignKey("vocabulary_table.id"))
+    method_type: Mapped["Vocabulary"] = relationship(back_populates="method", lazy="selectin")
+
+    device_id: Mapped[Optional[UUID]] = mapped_column(ForeignKey("device_table.id"))
+    device: Mapped["Device"] = relationship(back_populates="method", lazy="selectin")
