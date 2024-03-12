239c204f689f3bfbfa70c2dcda5da124d1bfbfde
hoangson <lehoangsonsg@gmail.com>
2024-03-12 11:19:35 +1030

    feat: make clear relationships to exclude for DTO

--- miappe/model/device.py
+++ miappe/model/device.py
@@ -17,10 +17,13 @@ class Device(Base):
     id: Mapped[UUID] = mapped_column(default=uuid4, primary_key=True)
     name: Mapped[str] = mapped_column(nullable=False)
     device_type_id: Mapped[UUID] = mapped_column(ForeignKey("vocabulary_table.id"))
-    device_type: Mapped["Vocabulary"] = relationship(back_populates="device")
+
     description: Mapped[Optional[str]]
     brand: Mapped[Optional[str]]
     serial_number: Mapped[Optional[str]]
     constructor_model: Mapped[Optional[str]]
     startup_date: Mapped[Optional[datetime.datetime]]
     removal_date: Mapped[Optional[datetime.datetime]]
+
+    # Relationships:
+    device_type: Mapped["Vocabulary"] = relationship(back_populates="device")
