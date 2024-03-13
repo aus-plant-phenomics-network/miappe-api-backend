ba594e949caa606cfe8dea6549fece86ac68f48f
harryle <lehoangsonsg@gmail.com>
2024-03-14 09:46:52 +1030

    feat: set audit columns to read-only #10

--- miappe/model/base.py
+++ miappe/model/base.py
@@ -1,14 +1,29 @@
-from uuid import uuid4, UUID
+from datetime import datetime, timezone
+from uuid import UUID, uuid4
 
-from advanced_alchemy.base import AuditColumns, CommonTableAttributes
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import Mapped, mapped_column
+from advanced_alchemy.base import CommonTableAttributes
+from advanced_alchemy.types import DateTimeUTC
+from litestar.dto import dto_field
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
-class Base(AuditColumns, CommonTableAttributes, DeclarativeBase):
+class Base(CommonTableAttributes, DeclarativeBase):
     id: Mapped[UUID] = mapped_column(default=uuid4, primary_key=True)
     name: Mapped[str] = mapped_column(nullable=True)
     description: Mapped[str] = mapped_column(nullable=True)
+
+    """Date/time of instance creation."""
+    created_at: Mapped[datetime] = mapped_column(
+        DateTimeUTC(timezone=True),
+        default=lambda: datetime.now(timezone.utc),
+        info=dto_field("read-only"),
+    )
+    """Date/time of instance last update."""
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTimeUTC(timezone=True),
+        default=lambda: datetime.now(timezone.utc),
+        info=dto_field("read-only"),
+    )
 
     def __repr__(self) -> str:
         return f"id: {self.id}, name: {self.name}, description: {self.description}"
--- miappe/model/device.py
+++ miappe/model/device.py
@@ -1,20 +1,20 @@
 import datetime
-from typing import Optional, TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional
 from uuid import UUID
 
 from sqlalchemy import ForeignKey
-from sqlalchemy.orm import mapped_column, Mapped, relationship
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from miappe.model.base import Base
 
 if TYPE_CHECKING:
     from miappe.model.method import Method
-    from miappe.model.vocabulary import Vocabulary
     from miappe.model.variable import Variable
+    from miappe.model.vocabulary import Vocabulary
 
 
 class Device(Base):
-    __tablename__ = "device_table"
+    __tablename__: str = "device_table"
 
     brand: Mapped[Optional[str]]
     serial_number: Mapped[Optional[str]]
@@ -24,11 +24,19 @@ class Device(Base):
 
     # Relationships:
     # With vocabulary
-    device_type_id: Mapped[Optional[UUID]] = mapped_column(ForeignKey("vocabulary_table.id"))
-    device_type: Mapped[Optional["Vocabulary"]] = relationship(back_populates="device", lazy="selectin")
+    device_type_id: Mapped[Optional[UUID]] = mapped_column(
+        ForeignKey("vocabulary_table.id")
+    )
+    device_type: Mapped[Optional["Vocabulary"]] = relationship(
+        back_populates="device", lazy="selectin"
+    )
 
     # With method
-    method: Mapped[Optional[list["Method"]]] = relationship(back_populates="device", lazy="selectin")
+    method: Mapped[Optional[list["Method"]]] = relationship(
+        back_populates="device", lazy="selectin"
+    )
 
     # With variable
-    variable: Mapped[Optional[list["Variable"]]] = relationship(back_populates="device", lazy="selectin")
+    variable: Mapped[Optional[list["Variable"]]] = relationship(
+        back_populates="device", lazy="selectin"
+    )
