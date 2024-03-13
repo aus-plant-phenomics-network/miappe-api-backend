0410c8e0d66cdd849f63b3bd10351d36f5c1b6b9
hoangson <lehoangsonsg@gmail.com>
2024-03-13 11:22:58 +1030

    feat: make name not required for update logic

--- miappe/model/base.py
+++ miappe/model/base.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 class Base(AuditColumns, CommonTableAttributes, DeclarativeBase):
     id: Mapped[UUID] = mapped_column(default=uuid4, primary_key=True)
-    name: Mapped[str] = mapped_column(nullable=False)
+    name: Mapped[str] = mapped_column(nullable=True)
     description: Mapped[str] = mapped_column(nullable=True)
 
     def __repr__(self) -> str:
