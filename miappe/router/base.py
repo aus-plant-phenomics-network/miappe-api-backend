27f03fefe23b74e3a29b70e79c7df446f986d5b1
harryle <lehoangsonsg@gmail.com>
2024-03-14 16:03:08 +1030

    chore: update annotation

--- miappe/router/base.py
+++ miappe/router/base.py
@@ -4,7 +4,12 @@ from uuid import UUID
 from litestar import Controller, get, put, post, delete
 from sqlalchemy.orm import DeclarativeBase
 
-from miappe.router.utils.CRUD import read_item_by_id, create_item, delete_item, update_item
+from miappe.router.utils.CRUD import (
+    read_item_by_id,
+    create_item,
+    delete_item,
+    update_item,
+)
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
@@ -12,23 +17,26 @@ if TYPE_CHECKING:
 
 
 class BaseController(Controller):
-    def __init__(self, table: DeclarativeBase, dto_factory: "DTOGenerator", *args, **kwargs):
+    def __init__(
+        self, table: DeclarativeBase, dto_struct: "DTOGenerator", *args, **kwargs
+    ):
         self.table = table
 
-        @get("/{id:uuid}", return_dto=dto_factory.read_dto)
+        @get("/{id:uuid}", return_dto=dto_struct.read_dto)
         async def _get_item_by_id(self, transaction: "AsyncSession", id: UUID) -> table:
             return await read_item_by_id(session=transaction, table=table, id=id)
 
-        @post(dto=dto_factory.write_dto, return_dto=dto_factory.write_dto)
-        async def _add_item(self, transaction: "AsyncSession", data: table) -> table:
+        @post(dto=dto_struct.write_dto, return_dto=dto_struct.write_dto)
+        async def _create_item(self, transaction: "AsyncSession", data: table) -> table:
             return await create_item(session=transaction, data=data)
 
-        @put("/{id:uuid}", dto=dto_factory.update_dto, return_dto=dto_factory.read_dto)
-        async def _update_item(self,
-                               transaction: "AsyncSession",
-                               id: UUID,
-                               data: table) -> table:
-            result = await update_item(session=transaction, id=id, data=data, table=table)
+        @put("/{id:uuid}", dto=dto_struct.update_dto, return_dto=dto_struct.read_dto)
+        async def _update_item(
+            self, transaction: "AsyncSession", id: UUID, data: table
+        ) -> table:
+            result = await update_item(
+                session=transaction, id=id, data=data, table=table
+            )
             return result
 
         @delete("/{id:uuid}")
@@ -36,7 +44,7 @@ class BaseController(Controller):
             await delete_item(session=transaction, id=id, table=table)
 
         self.get_item_by_id = _get_item_by_id
-        self.add_item = _add_item
+        self.create_item = _create_item
         self.update_item = _update_item
         self.delete_item = _delete_item
 
--- miappe/router/utils/DTO.py
+++ miappe/router/utils/DTO.py
@@ -7,25 +7,27 @@ from miappe.model import Device, Vocabulary, Method, Unit, Variable, BiologicalM
 
 
 class DTOGenerator:
-    def __init__(self,
-                 table: Any,
-                 read_exclude: set | None = None,
-                 write_exclude: set | None = None):
+    def __init__(
+        self,
+        table: Any,
+        read_exclude: set | None = None,
+        write_exclude: set | None = None,
+    ):
         self.table = table
         self.read_exclude = read_exclude if read_exclude is not None else set()
         self.write_exclude = write_exclude if write_exclude is not None else set()
 
         class GetDTO(SQLAlchemyDTO[table]):
-            config = SQLAlchemyDTOConfig(exclude=read_exclude)
+            config = SQLAlchemyDTOConfig(exclude=self.read_exclude, max_nested_depth=1)
 
         post_exclude = deepcopy(self.write_exclude)
-        post_exclude = post_exclude | {"id", "created_at", "updated_at"}
+        post_exclude = post_exclude | {"id"}
 
         class PostDTO(SQLAlchemyDTO[table]):
             config = SQLAlchemyDTOConfig(exclude=post_exclude)
 
         put_exclude = deepcopy(self.write_exclude)
-        put_exclude = put_exclude | {"id", "created_at", "updated_at"}
+        put_exclude = put_exclude | {"id"}
 
         class PutDTO(SQLAlchemyDTO[table]):
             config = SQLAlchemyDTOConfig(exclude=put_exclude)
@@ -35,64 +37,75 @@ class DTOGenerator:
         self.update_dto = PutDTO
 
 
-DeviceDTO = DTOGenerator(table=Device,
-                         read_exclude={"device_type.description",
-                                       "device_type.namespace",
-                                       "device_type.created_at",
-                                       "device_type.updated_at",
-                                       "device_type.external_reference",
-                                       "device_type.relationship_type",
-                                       "device_type.symbol",
-                                       "device_type.id",
-                                       "method",
-                                       "variable"},
-                         write_exclude={"device_type", "method", "variable"})
+DeviceDTO = DTOGenerator(
+    table=Device,
+    read_exclude={
+        "device_type.description",
+        "device_type.namespace",
+        "device_type.created_at",
+        "device_type.updated_at",
+        "device_type.external_reference",
+        "device_type.relationship_type",
+        "device_type.symbol",
+        "device_type.id",
+    },
+)
 
-VocabularyDTO = DTOGenerator(table=Vocabulary,
-                             read_exclude={"device", "method", "unit", "variable"},
-                             write_exclude={"device", "method", "unit", "variable"})
+VocabularyDTO = DTOGenerator(
+    table=Vocabulary,
+    read_exclude=None,
+    write_exclude=None,
+)
 
-MethodDTO = DTOGenerator(table=Method,
-                         read_exclude={"device",
-                                       "method_type.description",
-                                       "method_type.namespace",
-                                       "method_type.created_at",
-                                       "method_type.updated_at",
-                                       "method_type.external_reference",
-                                       "method_type.relationship_type",
-                                       "method_type.symbol",
-                                       "method_type.id"
-                                       },
-                         write_exclude={"device", "method_type"})
+MethodDTO = DTOGenerator(
+    table=Method,
+    read_exclude={
+        "device",
+        "method_type.description",
+        "method_type.namespace",
+        "method_type.created_at",
+        "method_type.updated_at",
+        "method_type.external_reference",
+        "method_type.relationship_type",
+        "method_type.symbol",
+        "method_type.id",
+    },
+    write_exclude={"device", "method_type"},
+)
 
-UnitDTO = DTOGenerator(table=Unit,
-                       read_exclude={
-                           "unit_type.description",
-                           "unit_type.namespace",
-                           "unit_type.created_at",
-                           "unit_type.updated_at",
-                           "unit_type.external_reference",
-                           "unit_type.relationship_type",
-                           "unit_type.symbol",
-                           "unit_type.id",
-                       },
-                       write_exclude={"unit_type"})
+UnitDTO = DTOGenerator(
+    table=Unit,
+    read_exclude={
+        "unit_type.description",
+        "unit_type.namespace",
+        "unit_type.created_at",
+        "unit_type.updated_at",
+        "unit_type.external_reference",
+        "unit_type.relationship_type",
+        "unit_type.symbol",
+        "unit_type.id",
+    },
+    write_exclude={"unit_type"},
+)
 
-VariableDTO = DTOGenerator(table=Variable,
-                           read_exclude={
-                               "variable_type.description",
-                               "variable_type.namespace",
-                               "variable_type.created_at",
-                               "variable_type.updated_at",
-                               "variable_type.external_reference",
-                               "variable_type.relationship_type",
-                               "variable_type.symbol",
-                               "variable_type.id",
-                               "device"
-                           },
-                           write_exclude={"variable_type", "device"}
-                           )
+VariableDTO = DTOGenerator(
+    table=Variable,
+    read_exclude={
+        "variable_type.description",
+        "variable_type.namespace",
+        "variable_type.created_at",
+        "variable_type.updated_at",
+        "variable_type.external_reference",
+        "variable_type.relationship_type",
+        "variable_type.symbol",
+        "variable_type.id",
+        "device",
+    },
+    write_exclude={"variable_type", "device"},
+)
 
-BiologicalMaterialDTO = DTOGenerator(table=BiologicalMaterial,
-                                     read_exclude={'preprocessing_method'},
-                                     write_exclude={'preprocessing_method'})
+BiologicalMaterialDTO = DTOGenerator(
+    table=BiologicalMaterial,
+    read_exclude={"preprocessing_method"},
+    write_exclude={"preprocessing_method"},
+)
