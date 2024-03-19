4f5b026f108953bba7715bf92d3d01711c18d7d3
hoangson <lehoangsonsg@gmail.com>
2024-03-19 14:55:29 +1030

    test: add test for Unit

---
+++ tests/router/test_unit.py
@@ -0,0 +1,23 @@
+from miappe.model import Unit
+from tests.helpers import AbstractBaseTestSuite, ResponseValidator
+
+
+class TestUnit(AbstractBaseTestSuite[Unit]):
+    path = "unit"
+    fixture = {"first": {"name": "first"},
+               "second": {"name": "second"},
+               "third": {"name": "third"}}
+    update_fixture = {"first": {"name": "first_unit"},
+                      "second": {"name": "second_unit"},
+                      "third": {"name": "third_unit"}}
+    invalid_create_fixture = {"first": {"name": "first"},
+                              "second": {"name": "second"},
+                              "third": {"name": "third"}}
+
+    async def test_get_item_by_name(self, test_client):
+        for key, value in self.fixture.items():
+            name = value['name']
+            async with test_client as client:
+                response = await client.get(self.path, params={"name": name})
+                ResponseValidator.validate_return_item_count(1, response)
+                ResponseValidator.validate_response_body(response.json()[0], name=name)
