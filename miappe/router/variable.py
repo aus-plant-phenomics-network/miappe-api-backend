0195caf1e4f2f852588623628bd85a83c045c0c9
hoangson <lehoangsonsg@gmail.com>
2024-03-19 15:07:50 +1030

    fix: update Variable router to use VariableDTO and allow to search with name #11

--- miappe/router/variable.py
+++ miappe/router/variable.py
@@ -1,13 +1,12 @@
-from typing import TYPE_CHECKING, Sequence
+from typing import Sequence
 
 from litestar import get
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from miappe.model import Variable
+from miappe.router.base import BaseController
 from miappe.router.base import read_items_by_attrs
 from miappe.router.utils.DTO import DTOGenerator
-
-from miappe.router.base import BaseController
 
 VariableDTO = DTOGenerator[Variable]()
 
@@ -19,8 +18,8 @@ class VariableController(BaseController[Variable]):
 
     @get(return_dto=VariableDTO.read_dto)
     async def get_variable(
-        self,
-        transaction: AsyncSession,
-        name: str|None=None
+            self,
+            transaction: AsyncSession,
+            name: str | None = None
     ) -> Sequence[Variable]:
         return await read_items_by_attrs(session=transaction, table=Variable, name=name)
