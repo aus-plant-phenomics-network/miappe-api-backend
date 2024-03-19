59272fb8bd2615b9c81bfa6e906670adfd8de78c
hoangson <lehoangsonsg@gmail.com>
2024-03-19 15:07:35 +1030

    fix: update Variable router to use VariableDTO and allow to search with name #11

--- miappe/router/variable.py
+++ miappe/router/variable.py
@@ -14,10 +14,13 @@ VariableDTO = DTOGenerator[Variable]()
 
 class VariableController(BaseController[Variable]):
     path = "/variable"
+    dto = VariableDTO.read_dto
+    return_dto = VariableDTO.write_dto
 
     @get(return_dto=VariableDTO.read_dto)
     async def get_variable(
         self,
         transaction: AsyncSession,
+        name: str|None=None
     ) -> Sequence[Variable]:
-        return await read_items_by_attrs(session=transaction, table=Variable)
+        return await read_items_by_attrs(session=transaction, table=Variable, name=name)
