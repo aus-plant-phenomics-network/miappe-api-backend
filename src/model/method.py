cbdfe1d533b06bcd68f195e3f3148ad8da062f5d
hoangson <lehoangsonsg@gmail.com>
2024-03-26 13:11:48 +1030

    feat: update method not back referencing device #27

--- src/model/method.py
+++ src/model/method.py
@@ -28,7 +28,7 @@ class Method(Base):
 
     device_id: Mapped[UUID | None] = mapped_column(ForeignKey("device_table.id"))
     device: Mapped[Optional["Device"]] = relationship(
-        back_populates="method", lazy="selectin", info=dto_field("read-only")
+        lazy="selectin", info=dto_field("read-only")
     )
 
     biological_material: Mapped[Optional["BiologicalMaterial"]] = relationship(
