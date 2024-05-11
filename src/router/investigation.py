3a852dc12ac132d799564700e5916a8362c3a2ae
harryle <lehoangsonsg@gmail.com>
2024-05-11 16:58:48 +0930

    feat: clean up investigation

--- src/router/investigation.py
+++ src/router/investigation.py
@@ -1,17 +1,9 @@
-from collections.abc import Sequence
-from typing import TYPE_CHECKING, Any
-
-from litestar import get
-from sqlalchemy.ext.asyncio import AsyncSession
-
 from src.model import Investigation
-from src.router.base import BaseController, read_items_by_attrs
+from src.router.base import BaseController
 from src.router.utils.dto import DTOGenerator
 
 __all__ = ("InvestigationController",)
 
-if TYPE_CHECKING:
-    pass
 
 InvestigationDTO = DTOGenerator[Investigation](
     read_kwargs={"max_nested_depth": 0, "rename_strategy": "camel"}, write_kwargs={"rename_strategy": "camel"}
@@ -22,7 +14,3 @@ class InvestigationController(BaseController[Investigation]):
     path = "/investigation"
     dto = InvestigationDTO.write_dto
     return_dto = InvestigationDTO.read_dto
-
-    @get("/")
-    async def get_items(self, table: Any, transaction: "AsyncSession", title: str | None) -> Sequence[Investigation]:
-        return await read_items_by_attrs(transaction, Investigation, title=title)
