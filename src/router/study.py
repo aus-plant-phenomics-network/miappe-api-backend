c67999d84eec45b7c73dfb7dc0632b1d04dd600c
hoangson <lehoangsonsg@gmail.com>
2024-05-27 09:01:48 +0930

    fix: fix bug due to nested depth causing implicity IO

--- src/router/study.py
+++ src/router/study.py
@@ -1,15 +1,11 @@
-from typing import TYPE_CHECKING
-
 from src.model import Study
 from src.router.base import BaseController
 from src.router.utils.dto import DTOGenerator
 
 __all__ = ("StudyController",)
 
-if TYPE_CHECKING:
-    pass
 
-StudyDTO = DTOGenerator[Study](read_kwargs={"max_nested_depth": 1})
+StudyDTO = DTOGenerator[Study]()
 
 
 class StudyController(BaseController[Study]):
