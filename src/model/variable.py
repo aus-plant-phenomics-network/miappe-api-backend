b921624927bc648d032dc9526d1e27dbd83e4051
hoangson <lehoangsonsg@gmail.com>
2024-05-26 15:42:27 +0930

    feat: update variable

--- src/model/variable.py
+++ src/model/variable.py
@@ -1,85 +1,81 @@
-# from typing import TYPE_CHECKING, Optional
-# from uuid import UUID
+from typing import TYPE_CHECKING, Optional
+from uuid import UUID
 
-# from litestar.dto import dto_field
-# from sqlalchemy import UUID as UUID_SQL
-# from sqlalchemy import Column, ForeignKey, Table
-# from sqlalchemy.orm import Mapped, mapped_column, relationship
+from litestar.dto import dto_field
+from sqlalchemy import UUID as UUID_SQL
+from sqlalchemy import Column, ForeignKey, Table
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-# from src.model.base import Base
+from src.model.base import Base
 
-# __all__ = ("Variable",)
-
-
-# if TYPE_CHECKING:
-#     from src.model.biological_material import BiologicalMaterial
-#     from src.model.device import Device
-#     from src.model.environment import Environment
-#     from src.model.facility import Facility
-#     from src.model.observation_unit import ObservationUnit
-#     from src.model.study import Study
-#     from src.model.vocabulary import Vocabulary
-
-# study_variable_table = Table(
-#     "study_variable_table",
-#     Base.metadata,
-#     Column("study_id", UUID_SQL, ForeignKey("study_table.id"), primary_key=True),
-#     Column("variable_id", UUID_SQL, ForeignKey("variable_table.id"), primary_key=True),
-# )
+__all__ = ("Variable",)
 
 
-# class Variable(Base):
-#     __tablename__: str = "variable_table"  # type: ignore[assignment]
+if TYPE_CHECKING:
+    # from src.model.biological_material import BiologicalMaterial
+    from src.model.device import Device
 
-#     time_interval: Mapped[str | None]
-#     sample_interval: Mapped[str | None]
+    # from src.model.facility import Facility
+    # from src.model.observation_unit import ObservationUnit
+    from src.model.study import Study
+    from src.model.unit import Unit
+    from src.model.vocabulary import Vocabulary
 
-#     # Relationships:
-#     variable_type_id: Mapped[UUID | None] = mapped_column(ForeignKey("vocabulary_table.id"))
-#     variable_type: Mapped[Optional["Vocabulary"]] = relationship(
-#         back_populates="variable", lazy="selectin", info=dto_field("read-only")
-#     )
+study_variable_table = Table(
+    "study_variable_table",
+    Base.metadata,
+    Column("study_id", UUID_SQL, ForeignKey("study_table.id"), primary_key=True),
+    Column("variable_id", UUID_SQL, ForeignKey("variable_table.id"), primary_key=True),
+)
 
-#     device_id: Mapped[UUID | None] = mapped_column(ForeignKey("device_table.id"))
-#     device: Mapped[Optional["Device"]] = relationship(lazy="selectin", info=dto_field("read-only"))
 
-#     # Sub-variable relationship
-#     biological_material: Mapped[Optional["BiologicalMaterial"]] = relationship(
-#         back_populates="variable", lazy="selectin", info=dto_field("read-only")
-#     )
-#     environment: Mapped[Optional["Environment"]] = relationship(
-#         back_populates="variable", lazy="selectin", info=dto_field("read-only")
-#     )
+class Variable(Base):
+    __tablename__: str = "variable_table"  # type: ignore[assignment]
 
-#     # Variable to study relationship
-#     studies: Mapped[list["Study"]] = relationship(
-#         "Study",
-#         secondary="study_variable_table",
-#         back_populates="variables",
-#         lazy="selectin",
-#         info=dto_field("read-only"),
-#     )
+    time_interval: Mapped[str | None]
+    sample_interval: Mapped[str | None]
 
-#     facilities: Mapped[list["Facility"]] = relationship(
-#         "Facility",
-#         secondary="facility_variable_table",
-#         back_populates="variables",
-#         lazy="selectin",
-#         info=dto_field("read-only"),
-#     )
+    # Relationships:
+    variable_type_id: Mapped[UUID | None] = mapped_column(ForeignKey("vocabulary_table.id", ondelete="SET NULL"))
+    variable_type: Mapped[Optional["Vocabulary"]] = relationship(
+        back_populates="variable", lazy=None, info=dto_field("read-only")
+    )
 
-#     observation_unit_biological_materials: Mapped[list["ObservationUnit"]] = relationship(
-#         "ObservationUnit",
-#         back_populates="biological_material",
-#         lazy="selectin",
-#         info=dto_field("read-only"),
-#         primaryjoin="Variable.id== ObservationUnit.biological_material_id",
-#     )
+    device_id: Mapped[UUID | None] = mapped_column(ForeignKey("device_table.id", ondelete="SET NULL"))
+    device: Mapped[Optional["Device"]] = relationship(lazy=None, info=dto_field("read-only"))
 
-#     observation_unit_factors: Mapped[list["ObservationUnit"]] = relationship(
-#         "ObservationUnit",
-#         back_populates="factor",
-#         lazy="selectin",
-#         info=dto_field("read-only"),
-#         primaryjoin="Variable.id == ObservationUnit.factor_id",
-#     )
+    unit_id: Mapped[UUID | None] = mapped_column(ForeignKey("unit_table.id", ondelete="SET NULL"))
+    unit: Mapped[Optional["Unit"]] = relationship(lazy=None, info=dto_field("read-only"))
+
+    # Variable to study relationship
+    studies: Mapped[list["Study"]] = relationship(
+        "Study",
+        secondary="study_variable_table",
+        back_populates="variables",
+        lazy=None,
+        info=dto_field("read-only"),
+    )
+
+    # facilities: Mapped[list["Facility"]] = relationship(
+    #     "Facility",
+    #     secondary="facility_variable_table",
+    #     back_populates="variables",
+    #     lazy="selectin",
+    #     info=dto_field("read-only"),
+    # )
+
+    # observation_unit_biological_materials: Mapped[list["ObservationUnit"]] = relationship(
+    #     "ObservationUnit",
+    #     back_populates="biological_material",
+    #     lazy="selectin",
+    #     info=dto_field("read-only"),
+    #     primaryjoin="Variable.id== ObservationUnit.biological_material_id",
+    # )
+
+    # observation_unit_factors: Mapped[list["ObservationUnit"]] = relationship(
+    #     "ObservationUnit",
+    #     back_populates="factor",
+    #     lazy="selectin",
+    #     info=dto_field("read-only"),
+    #     primaryjoin="Variable.id == ObservationUnit.factor_id",
+    # )
