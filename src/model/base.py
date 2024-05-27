bf153fb5eb5a7643548dfe3e9a536e6d6fd76cb7
hoangson <lehoangsonsg@gmail.com>
2024-05-27 14:31:02 +0930

    feat: add generalised base dataclass

--- src/model/base.py
+++ src/model/base.py
@@ -1,4 +1,7 @@
+import abc
+from dataclasses import asdict, dataclass
 from datetime import UTC, datetime
+from typing import Any, Protocol
 from uuid import UUID, uuid4
 
 from advanced_alchemy.base import CommonTableAttributes
@@ -6,7 +9,12 @@ from advanced_alchemy.types import DateTimeUTC
 from litestar.dto import dto_field
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
-__all__ = ("Base",)
+__all__ = ("Base", "BaseDataclass")
+
+
+class Serialisable(Protocol):
+    def to_dict(self, exclude: set[str] | None = None) -> dict[str, Any]:
+        return {}
 
 
 class Base(CommonTableAttributes, DeclarativeBase):
@@ -24,3 +32,28 @@ class Base(CommonTableAttributes, DeclarativeBase):
         default=lambda: datetime.now(UTC),
         info=dto_field("read-only"),
     )
+
+    def to_dict(self, exclude: set[str] | None = None) -> dict[str, Any]:
+        data_dict = super().to_dict(exclude)
+        data_dict["updated_at"] = datetime.now(UTC)
+        return {k: v for k, v in data_dict.items() if v is not None}
+
+
+@dataclass
+class BaseDataclass:
+    id: UUID | None
+    title: str
+    created_at: datetime | None
+    updated_at: datetime | None
+
+    def to_dict(self, exclude: set[str] | None = None) -> dict[str, Any]:
+        result = {k: v for k, v in asdict(self).items() if v is not None}
+        if exclude:
+            return {k: v for k, v in result.items() if k not in exclude}
+        return result
+
+    @classmethod
+    @abc.abstractmethod
+    def from_orm(cls, data: Serialisable) -> "BaseDataclass":
+        data_dict = data.to_dict()
+        return cls(**data_dict)
