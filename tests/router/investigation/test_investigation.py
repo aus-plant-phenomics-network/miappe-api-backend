e4343aa1d24a9dd787ff3b83015128b0093d38ce
hoangson <lehoangsonsg@gmail.com>
2024-05-28 10:14:39 +0930

    test: add investigation integerity test

--- tests/router/investigation/test_investigation.py
+++ tests/router/investigation/test_investigation.py
@@ -1,7 +1,7 @@
 from httpx import Response
 from litestar.testing import AsyncTestClient
 
-from tests.helpers import delete_fixture, validate_get_not_exist, validate_post, validate_put
+from tests.helpers import delete_fixture, post_fixture, validate_get_not_exist, validate_post, validate_put
 from tests.router.investigation.conftest import (
     BARLEY_PROJECT_INVESTIGATION,
     FIRST_PROJECT,
@@ -23,6 +23,13 @@ async def test_delete_first_project(setup_first_project: Response, test_client: 
     response = await delete_fixture(PATH, setup_first_project.json()["id"], test_client)
     assert response.status_code == 204
     await validate_get_not_exist(PATH, test_client, setup_first_project.json()["id"])
+
+
+async def test_create_project_same_name_throws_error(
+    setup_first_project: Response, test_client: AsyncTestClient
+) -> None:
+    response = await post_fixture(PATH, FIRST_PROJECT, test_client)
+    assert response.status_code == 409
 
 
 async def test_barley_project_created(setup_barley_project: Response, test_client: AsyncTestClient) -> None:
