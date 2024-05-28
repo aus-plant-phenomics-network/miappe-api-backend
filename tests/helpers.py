1e26257dae6da4a04b1b630c32acd9755e15d920
hoangson <lehoangsonsg@gmail.com>
2024-05-28 10:06:43 +0930

    feat: update test helper methods

---
+++ tests/helpers.py
@@ -0,0 +1,81 @@
+import datetime
+from typing import Any, Literal
+from uuid import UUID
+
+from httpx import Response
+from litestar.testing import AsyncTestClient
+
+from src.model.base import Serialisable
+
+
+def serialise_datetime(obj: Any) -> str:
+    if isinstance(obj, datetime.datetime):
+        return obj.strftime("%Y-%m-%d")
+    return str(obj)
+
+
+ScopeValue = Literal["function", "module", "class", "package"]
+
+
+def snake_to_camel(snake_str: str) -> str:
+    # Split the snake_case string into words
+    components = snake_str.split("_")
+    # Capitalize the first letter of each word except the first one and join them together
+    return components[0] + "".join(x.title() for x in components[1:])
+
+
+def serialise_data(data: dict[str, Any] | Serialisable) -> dict[str, Any]:
+    dict_data = data if isinstance(data, dict) else data.to_dict()
+    new_dict = {}
+    for k, v in dict_data.items():
+        new_k = snake_to_camel(k)
+        if isinstance(v, datetime.datetime):
+            new_dict[new_k] = v.strftime("%Y-%m-%dT%H:%M:%S") if k == "updated_at" else v.isoformat()
+        else:
+            new_dict[new_k] = str(v)
+    return new_dict
+
+
+async def delete_fixture(path: str, id: UUID, client: AsyncTestClient) -> Response:
+    return await client.delete(f"{path}/{id}")
+
+
+async def post_fixture(path: str, data: dict[str, Any] | Serialisable, client: AsyncTestClient) -> Response:
+    return await client.post(path, json=serialise_data(data))
+
+
+async def put_fixture(path: str, data: dict[str, Any] | Serialisable, client: AsyncTestClient, id: UUID) -> Response:
+    return await client.put(f"{path}/{id}", json=serialise_data(data))
+
+
+async def validate_get_exist(path: str, data: dict[str, Any] | Serialisable, client: AsyncTestClient, id: UUID) -> None:
+    query = await client.get(f"{path}/{id}")
+    assert query.status_code == 200
+    query_data = query.json()
+    for k, v in serialise_data(data).items():
+        assert k in query_data
+        if k == "updatedAt":
+            assert v == query_data[k][:19]
+        else:
+            assert v == query_data[k]
+
+
+async def validate_get_not_exist(path: str, client: AsyncTestClient, id: UUID) -> None:
+    query = await client.get(f"{path}/{id}")
+    assert query.status_code == 404
+
+
+async def validate_post(
+    path: str, data: dict[str, Any] | Serialisable, client: AsyncTestClient, response: Response
+) -> None:
+    assert response.status_code == 201
+    id = response.json()["id"]
+    await validate_get_exist(path, data, client, id)
+
+
+async def validate_put(
+    path: str, data: dict[str, Any] | Serialisable, client: AsyncTestClient, response: Response
+) -> None:
+    assert response.status_code == 200
+    id = response.json()["id"]
+    await validate_get_exist(path, data, client, id)
--- tests/router/conftest.py
+++ tests/router/conftest.py
@@ -4,45 +4,6 @@ from uuid import UUID
 import pytest
 from litestar.testing import AsyncTestClient
 
-PROJECT_INIT_TITLE = "First Project"
-PROJECT_UPDATED_TITLE = "First Investigation"
-PROJECT_UPDATED_DESCRIPTION = "Project Description"
-
-# Investigation fixture
-
-
-@pytest.fixture(scope="function")
-async def setup_investigation(test_client: AsyncTestClient) -> AsyncGenerator[UUID, None]:
-    response = await test_client.post("/investigation", json={"title": PROJECT_INIT_TITLE})
-    assert response.status_code == 201
-    response_id = response.json()["id"]
-    yield response_id
-
-    await test_client.delete(f"investigation/{response_id}")
-
-
-@pytest.fixture(scope="function")
-async def setup_investigation_no_cleanup(test_client: AsyncTestClient) -> AsyncGenerator[UUID, None]:
-    response = await test_client.post(
-        "/investigation", json={"title": PROJECT_UPDATED_TITLE, "description": PROJECT_UPDATED_DESCRIPTION}
-    )
-    assert response.status_code == 201
-    response_id = response.json()["id"]
-    yield response_id
-
-
-@pytest.fixture(scope="function")
-async def setup_investigation_with_cleanup(test_client: AsyncTestClient) -> AsyncGenerator[UUID, None]:
-    response = await test_client.post(
-        "/investigation", json={"title": PROJECT_UPDATED_TITLE, "description": PROJECT_UPDATED_DESCRIPTION}
-    )
-    assert response.status_code == 201
-    response_id = response.json()["id"]
-    yield response_id
-
-    await test_client.delete(f"investigation/{response_id}")
-
-
 # Study fixture
 STUDY_INIT_TITLE = "First study"
 STUDY_INIT_OBJECTIVE = "Study Objective"
