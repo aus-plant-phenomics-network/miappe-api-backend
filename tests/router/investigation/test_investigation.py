948000f4e5b22e84a35b079a7c344dcd05fe1b90
hoangson <lehoangsonsg@gmail.com>
2024-05-28 10:06:15 +0930

    test: move investigation to a separate module and update tests

---
+++ tests/router/investigation/test_investigation.py
@@ -0,0 +1,33 @@
+from httpx import Response
+from litestar.testing import AsyncTestClient
+
+from tests.helpers import delete_fixture, validate_get_not_exist, validate_post, validate_put
+from tests.router.investigation.conftest import (
+    BARLEY_PROJECT_INVESTIGATION,
+    FIRST_PROJECT,
+    FIRST_PROJECT_UPDATED,
+    MAIZE_PROJECT_INVESTIGATION,
+    PATH,
+)
+
+
+async def test_first_project_created(setup_first_project: Response, test_client: AsyncTestClient) -> None:
+    await validate_post(PATH, FIRST_PROJECT, test_client, setup_first_project)
+
+
+async def test_first_project_updated(update_first_project: Response, test_client: AsyncTestClient) -> None:
+    await validate_put(PATH, FIRST_PROJECT_UPDATED, test_client, update_first_project)
+
+
+async def test_delete_first_project(setup_first_project: Response, test_client: AsyncTestClient) -> None:
+    response = await delete_fixture(PATH, setup_first_project.json()["id"], test_client)
+    assert response.status_code == 204
+    await validate_get_not_exist(PATH, test_client, setup_first_project.json()["id"])
+
+
+async def test_barley_project_created(setup_barley_project: Response, test_client: AsyncTestClient) -> None:
+    await validate_post(PATH, BARLEY_PROJECT_INVESTIGATION, test_client, setup_barley_project)
+
+
+async def test_maize_project_created(setup_maize_project: Response, test_client: AsyncTestClient) -> None:
+    await validate_post(PATH, MAIZE_PROJECT_INVESTIGATION, test_client, setup_maize_project)
