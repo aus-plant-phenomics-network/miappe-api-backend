a1fb5ada6be8da6ca231a82f0b5195c126ea690c
harryle <lehoangsonsg@gmail.com>
2024-05-29 14:05:12 +0930

    feat: add environment variable to setup

--- src/setup.py
+++ src/setup.py
@@ -5,6 +5,18 @@ from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from src.model import Base
 from tests.router.data_file.fixture import BARLEY_DATA_FILE, MAIZE_DATA_FILE
 from tests.router.device.fixture import SCANALYZER_DEVICE, SCANALYZER_TYPE
+from tests.router.environment.fixture import (
+    BARLEY_FERTILIZER,
+    BARLEY_LIGHT_INTENSITY,
+    BARLEY_RELATIVE_HUMIDITY,
+    BARLEY_ROOTING_MEDIUM,
+    BARLEY_SOWING_DENSITY,
+    BARLEY_TEMPERATURE,
+    BARLEY_WATERING_EXPOSURE,
+    MAIZE_PH,
+    MAIZE_ROOTING_MEDIUM,
+    MAIZE_SOWING_DENSITY,
+)
 from tests.router.institution.fixture import ANU, APPN, INRAE, RESEARCH_INSTITUTE, TPA, UMR, UNIVERSITY, UOA, UPARIS
 from tests.router.investigation.fixture import BARLEY_PROJECT_INVESTIGATION, FIRST_PROJECT, MAIZE_PROJECT_INVESTIGATION
 from tests.router.method.fixture import (
@@ -73,6 +85,32 @@ async def async_main() -> None:
         # Add unit
         DEGREE_DAY_UNIT.unit_reference = DEGREE_DAY_REF
         session.add_all([DEGREE_DAY_REF, DEGREE_DAY_UNIT, MICROGRAM_UNIT, KILO_PIXEL_UNIT])
+
+        # Add environment variable
+        MAIZE_SOWING_DENSITY.studies = [MAIZE_PROJECT_STUDY]
+        MAIZE_PH.studies = [MAIZE_PROJECT_STUDY]
+        MAIZE_ROOTING_MEDIUM.studies = [MAIZE_PROJECT_STUDY]
+        BARLEY_FERTILIZER.studies = [BARLEY_PROJECT_STUDY]
+        BARLEY_ROOTING_MEDIUM.studies = [BARLEY_PROJECT_STUDY]
+        BARLEY_TEMPERATURE.studies = [BARLEY_PROJECT_STUDY]
+        BARLEY_LIGHT_INTENSITY.studies = [BARLEY_PROJECT_STUDY]
+        BARLEY_RELATIVE_HUMIDITY.studies = [BARLEY_PROJECT_STUDY]
+        BARLEY_SOWING_DENSITY.studies = [BARLEY_PROJECT_STUDY]
+        BARLEY_WATERING_EXPOSURE.studies = [BARLEY_PROJECT_STUDY]
+        session.add_all(
+            [
+                BARLEY_FERTILIZER,
+                BARLEY_LIGHT_INTENSITY,
+                BARLEY_RELATIVE_HUMIDITY,
+                BARLEY_ROOTING_MEDIUM,
+                BARLEY_SOWING_DENSITY,
+                BARLEY_TEMPERATURE,
+                BARLEY_WATERING_EXPOSURE,
+                MAIZE_PH,
+                MAIZE_ROOTING_MEDIUM,
+                MAIZE_SOWING_DENSITY,
+            ]
+        )
 
         await session.commit()
 
