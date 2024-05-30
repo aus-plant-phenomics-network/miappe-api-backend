1da7c8a20e020bbfbfadd9a2281864e3be14a683
hoangson <lehoangsonsg@gmail.com>
2024-05-30 21:08:13 +0930

    feat: update router to reflect schema

--- src/router/experiment.py
+++ src/router/experiment.py
@@ -2,6 +2,7 @@ from litestar.dto import DataclassDTO, DTOConfig
 
 from src.model.experiment import Experiment, ExperimentDataclass
 from src.model.facility import Facility
+from src.model.staff import Staff
 from src.router.base import DataclassController
 from src.router.utils.dto import DTOGenerator
 
@@ -19,5 +20,5 @@ class ExperimentController(DataclassController[Experiment, ExperimentDataclass])
     path = "/experiment"
     dto = ExperimentDTO
     return_dto = ExperimentDTO
-    attr_map = {"facilities": ("facility_id", Facility)}
-    selectinload_attrs = [Experiment.facilities]
+    attr_map = {"facilities": ("facility_id", Facility), "staffs": ("staff_id", Staff)}
+    selectinload_attrs = [Experiment.facilities, Experiment.staffs]
