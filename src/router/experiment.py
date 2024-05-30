de6ea0ebc5d77b14a32e3a2065c5f881259c3324
hoangson <lehoangsonsg@gmail.com>
2024-05-30 17:58:50 +0930

    feat: update experiment router

--- src/router/experiment.py
+++ src/router/experiment.py
@@ -1,14 +1,23 @@
-from src.model import Experiment
-from src.router.base import BaseController
+from litestar.dto import DataclassDTO, DTOConfig
+
+from src.model.experiment import Experiment, ExperimentDataclass
+from src.model.facility import Facility
+from src.router.base import DataclassController
 from src.router.utils.dto import DTOGenerator
 
 __all__ = ("ExperimentController",)
 
 
-ExperimentDTO = DTOGenerator[Experiment]()
+class ExperimentDTO(DataclassDTO[ExperimentDataclass]):
+    config = DTOConfig(rename_strategy="camel")
 
 
-class ExperimentController(BaseController[Experiment]):
+ExperimentReturnDTO = DTOGenerator[Experiment]()
+
+
+class ExperimentController(DataclassController[Experiment, ExperimentDataclass]):
     path = "/experiment"
-    dto = ExperimentDTO.write_dto
-    return_dto = ExperimentDTO.read_dto
+    dto = ExperimentDTO
+    return_dto = ExperimentDTO
+    attr_map = {"facilities": ("facility_id", Facility)}
+    selectinload_attrs = [Experiment.facilities]
