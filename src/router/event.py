8466f23479b38552ca63756bc310c4dc0e576e0f
harryle <lehoangsonsg@gmail.com>
2024-06-02 19:39:28 +0930

    feat: update event router

--- src/router/event.py
+++ src/router/event.py
@@ -1,14 +1,27 @@
-from src.model import Event
-from src.router.base import BaseController
+from litestar.dto import DataclassDTO, DTOConfig
+
+from src.model.event import Event, EventDataclass
+from src.model.observation_unit import ObservationUnit
+from src.model.study import Study
+from src.router.base import DataclassController
 from src.router.utils.dto import DTOGenerator
 
 __all__ = ("EventController",)
 
 
-EventDTO = DTOGenerator[Event]()
+class EventDTO(DataclassDTO[EventDataclass]):
+    config = DTOConfig(rename_strategy="camel")
 
 
-class EventController(BaseController[Event]):
+EventReturnDTO = DTOGenerator[Event]()
+
+
+class EventController(DataclassController[Event, EventDataclass]):
     path = "/event"
-    dto = EventDTO.write_dto
-    return_dto = EventDTO.read_dto
+    dto = EventDTO
+    return_dto = EventDTO
+    attr_map = {
+        "studies": ("study_id", Study),
+        "observation_units": ("observation_unit_id", ObservationUnit),
+    }
+    selectinload_attrs = [Event.studies, Event.observation_units]
