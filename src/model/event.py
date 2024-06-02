fc2f3e1098ac3287e2a09981ad0dbd78732a2a9a
harryle <lehoangsonsg@gmail.com>
2024-06-02 19:39:21 +0930

    feat: update event schema

--- src/model/event.py
+++ src/model/event.py
@@ -1,5 +1,6 @@
 import datetime
-from typing import TYPE_CHECKING, Optional
+from dataclasses import dataclass, field
+from typing import TYPE_CHECKING, Optional, cast
 from uuid import UUID
 
 from litestar.dto import dto_field
@@ -7,7 +8,7 @@ from sqlalchemy import UUID as UUID_SQL
 from sqlalchemy import Column, ForeignKey, Table
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from src.model import Base
+from src.model.base import Base, BaseDataclass, Serialisable
 
 if TYPE_CHECKING:
     from src.model.observation_unit import ObservationUnit
@@ -20,23 +21,9 @@ __all__ = ("Event",)
 event_to_ob_unit_table = Table(
     "event_to_ob_unit_table",
     Base.metadata,
+    Column("event_id", UUID_SQL, ForeignKey("event_table.id", ondelete="cascade"), primary_key=True),
     Column(
-        "event_id",
-        UUID_SQL,
-        ForeignKey(
-            "event_table.id",
-            ondelete="cascade",
-        ),
-        primary_key=True,
-    ),
-    Column(
-        "observation_unit_id",
-        UUID_SQL,
-        ForeignKey(
-            "observation_unit_table.id",
-            ondelete="cascade",
-        ),
-        primary_key=True,
+        "observation_unit_id", UUID_SQL, ForeignKey("observation_unit_table.id", ondelete="cascade"), primary_key=True
     ),
 )
 
@@ -76,3 +63,21 @@ class Event(Base):
         info=dto_field("read-only"),
         back_populates="events",
     )
+
+
+@dataclass(kw_only=True)
+class EventDataclass(BaseDataclass):
+    event_date: datetime.datetime | None = field(default=None)
+    event_type_id: UUID | None = field(default=None)
+    observation_unit_id: list[UUID] = field(default_factory=list[UUID])
+    studies: list[UUID] = field(default_factory=list[UUID])
+
+    @classmethod
+    def from_orm(cls, data: Serialisable) -> "BaseDataclass":
+        data = cast(Event, data)
+        data_dict = data.to_dict()
+        if len(data.observation_units) > 0:
+            data_dict["observation_unit_id"] = [item.id for item in data.observation_units]
+        if len(data.studies) > 0:
+            data_dict["study_id"] = [item.id for item in data.studies]
+        return cls(**data_dict)
